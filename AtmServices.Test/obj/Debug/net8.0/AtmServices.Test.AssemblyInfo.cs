e2d437b361ef67e5f3a13aceb6b45976f1d166eb
Dane Bishop <115124397+danebishop@users.noreply.github.com>
2025-02-06 17:19:55 +0000

    .dll added to gitignore

--- AtmServices.Test/obj/Debug/net8.0/AtmServices.Test.AssemblyInfo.cs
+++ AtmServices.Test/obj/Debug/net8.0/AtmServices.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtmServices.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8baa9a36bc88859238e1881a3f24bb9d4e2ec351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27dbc7a75396ecc45d7ed1992ef9139254db059d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtmServices.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtmServices.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
